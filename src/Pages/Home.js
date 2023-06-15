7d546e1d2c6b422f6a824a64d3b717850aaab94d
Kesav-21 <cgkesav@gmail.com>
2023-06-15 23:04:03 +0530

    homepage and github links updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -10,20 +10,18 @@ const Home=()=>{
                 <div className="text-section">
                     <div className="content">
                         <h1 id="tname">Kesavaraman</h1>
-                        <h3>Web Developer</h3>
+                        <h3>MERN Stack Developer</h3>
                         <div className="image-section">
                     <img src={K} alt="kesav-profile"/>
                 </div>
-                        <p>Hi! I'm Kesav. I'm a Tech enthusiast who enjoys web development, coding, love to learn something new.I'm passionate self-taught front-end developer.</p>
+                        <p>Hi! I'm Kesavaraman, a passionate self-taught Full Stack web developer and a graduand in Computer Science from Panimalar Engineering College. I'm a Tech enthusiast and a passionate learner who's always willing to learn and work across technologies and domains bringing you programming and design from the future.</p>
                         <div class="buttondiv">
                             <a className="button" href="https://Kesav-21.github.io/ProfilePage">Profile Page</a>
                         <a className="button" href="https://Kesav-21.github.io/Kesav-CV">Resume</a>
                         <Link className="button" to="/skills">My Skills</Link>
                         <Link className="button" to="/certification">Certification</Link>
                         <Link className="button" to="/works">My Works</Link>
-                        <Link className="button" to="/contactme">Contact Me</Link>
-                        
-                        
+                        <Link className="button" to="/contactme">Contact Me</Link>                        
                         </div>
                     </div>
                 </div> 
